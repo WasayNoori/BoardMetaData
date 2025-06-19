4d10ddc0ab9de0eae946b19ddb8290cba823d813
WasayNoori <wasay@hawkridgesys.com>
2025-06-18 23:49:01 -0700

    working within board

---
+++ BoardDataView/Pages/BoardMeta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BoardDataView.Pages
+{
+    public class BoardMetaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BoardDataView/Pages/Shared/BoardView.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BoardDataView.Pages.Shared
+{
+    public class BoardViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+
+
+}
